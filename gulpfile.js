9f072d9802acefcdb3369a77e246deafff7d8400
Anthony Vergara <ajvergara2326@gmail.com>
2018-03-23 13:31:09 +0300

    Too many commits from previous lessons - sorry will not happen again

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,2 @@
-const gulp = require("gulp"),
-      watch = require("gulp-watch");
-
-gulp.task("default", function(){
-  console.log("Default task");
-});
-
-gulp.task("html", function(){
-  console.log("HTML has been changed");
-});
-
-gulp.task("styles", function(){
-  console.log("CSS files has been changed");
-});
-
-gulp.task("watch", function(){
-  watch("./app/index.html", function(){
-    gulp.start("html");
-  });
-
-  watch("./app/assets/styles/**/*.css", function(){
-    gulp.start("styles");
-  });
-});
+require("./gulp/tasks/styles");
+require("./gulp/tasks/watch");
