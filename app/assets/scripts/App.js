c36d7f368135eee93de54018216a8662a696d235
Anthony Vergara <ajvergara2326@gmail.com>
2018-04-04 22:03:34 +0300

    Click event on Menu Icon has been completed - next aligning menu on mobile screen

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,15 +1,3 @@
-const $      = require("jquery");
-import Person from "./modules/Person";
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person{
-  payTaxes(){
-    console.log(this.name + " now owes $12 in Taxes");
-  };
-};
-
-const john = new Person("John Doe", "blue");
-john.greet();
-
-const jane = new Adult("Jane Smith", "red");
-jane.greet();
-jane.payTaxes();
+const mobileMenu = new MobileMenu;
