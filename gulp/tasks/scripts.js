a088429a020d4dfe4b9189de6600aaa66e52fc3b
Anthony Vergara <ajvergara2326@gmail.com>
2018-04-10 21:25:42 +0300

    Lazysizes applied next Waypoint fixes

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -4,10 +4,13 @@ const gulp = require("gulp"),
 gulp.task("scripts", function(){
   webpack({
       mode: "development",
-      entry: __dirname  + "/../../app/assets/scripts/App.js",
+      entry: {
+        App: __dirname  + "/../../app/assets/scripts/App.js",
+        Vendor: __dirname + "/../../app/assets/scripts/Vendor.js"
+      },
       output: {
         path: __dirname +  "/../../app/temp/scripts",
-        filename: "App.js"
+        filename: "[name].js"
       },
       module: {
         rules: [
